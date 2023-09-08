fad3e7bd1c888da1aa02e77842fa175c1d289d84
JSYRD <1353097778@qq.com>
2023-09-08 15:56:15 +0200

    fix&feat: reconstructed page; adding new function

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'dart:async';
 import 'dart:convert';
-
-import 'package:dartzmq/dartzmq.dart';
 import 'package:ds_queue_studentclient/config.dart';
 import 'package:ds_queue_studentclient/listinfo.dart';
 import 'package:flutter/material.dart';
 import 'package:ds_queue_studentclient/utils.dart';
-import 'dart:isolate';
 
 void main() {
   runApp(const MyApp());
@@ -15,21 +11,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(
@@ -42,27 +28,17 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
   final String title;
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final StudentContainer _studentContainer = StudentContainer();
   List<ListInfo> _queue = [];
-
-  final ZContext _context = ZContext();
-  late final ZSocket _socket;
-  late StreamSubscription _subscription;
+  List<ListInfo> _supervisors = [];
+  ZMQHelper zmqHelper = ZMQHelper();
+  final TextEditingController _nameController = TextEditingController();
 
   List<Text> wrapName2Text(List<String> names) {
     List<Text> ret = [];
@@ -72,79 +48,197 @@ class _MyHomePageState extends State<MyHomePage> {
     return ret;
   }
 
-  @override
-  void initState() {
-    _socket = _context.createSocket(SocketType.sub);
-    _socket.connect(Config.url);
-    _socket.subscribe("queue");
-    // print("test");
-    _subscription = _socket.messages.listen((event) {
-      var queue = json.decode(utf8.decode(event.last.payload));
-      setState(() {
+  void subscribeQueueStatus() {
+    zmqHelper.subscribe(
+      Config.url,
+      "queue",
+      (event) {
+        var queue = json.decode(utf8.decode(event.last.payload));
         List<ListInfo> newqueue = [];
         for (var element in queue) {
           newqueue.add(ListInfo(
               title: element['name'], data: "ticket:${element['ticket']}"));
         }
-        _queue = newqueue;
-      });
-    });
+        setState(
+          () {
+            _queue = newqueue;
+          },
+        );
+      },
+    );
+  }
+
+  void subscribeSupervisorStatus() {
+    zmqHelper.subscribe(
+      Config.url,
+      "supervisors",
+      (event) {
+        var supervisors = json.decode(utf8.decode(event.last.payload));
+        List<ListInfo> newqueue = [];
+        for (var element in supervisors) {
+          newqueue.add(ListInfo(
+              title: element['name'],
+              data:
+                  "status:${element['status']}    ${element['client'] == 'undefined' ? 'No Client.' : "Client: ${element['client']['name']} Ticket: ${element['client']['ticket']} "}"));
+        }
+        setState(
+          () {
+            _supervisors = newqueue;
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    subscribeQueueStatus();
+    subscribeSupervisorStatus();
     super.initState();
   }
 
   @override
   void dispose() {
-    _socket.close();
-    _context.stop();
-    _subscription.cancel();
+    zmqHelper.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    var appBarTitle = Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Padding(
-          padding: EdgeInsets.all(2.0),
-          child: Icon(Icons.format_line_spacing_rounded),
-        ),
-        Text(widget.title)
-      ],
-    );
-
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: appBarTitle,
-      ),
-      // body: Column(
-      //   children: wrapName2Text(_studentContainer.getAllNames()),
-      // ),
-      body: ListView(children: [
-        const Text(
-          'Current Queue:',
-          style: TextStyle(fontSize: 32),
+      drawer: const Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.all(0.0),
+              child: Center(
+                child: Text("About"),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('Designed By'),
+                  Text('RundongYang & Leon'),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 20.0),
+                    child: Text('University of Skövde'),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
-        Column(
-          children: _queue,
-        )
-      ]),
-      floatingActionButton: IconButton(
-        icon: const Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            _studentContainer
-                .add(Student(name: "Rundong Yang", id: 114, number: 514));
-          });
-        },
+      ),
+      body: Builder(
+        builder: (context) => Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Row(
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Scaffold.of(context).openDrawer();
+                    },
+                    child: const Icon(Icons.menu_open),
+                  )
+                ],
+              ),
+            ),
+            const Divider(
+              height: 1.0,
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: ListView(children: [
+                      const Padding(
+                        padding: EdgeInsets.only(left: 20.0, top: 20.0),
+                        child: Text(
+                          'Current Queue:',
+                          style: TextStyle(fontSize: 32),
+                        ),
+                      ),
+                      Column(
+                        children: _queue.isEmpty
+                            ? [const Text("No Student in Queue Currently.")]
+                            : _queue,
+                      )
+                    ]),
+                  ),
+                  const VerticalDivider(
+                    width: 1,
+                  ),
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 20.0, top: 20.0),
+                          child: Text(
+                            'Current Supervisors:',
+                            style: TextStyle(fontSize: 32),
+                          ),
+                        ),
+                        Column(
+                          children: _supervisors.isEmpty
+                              ? [const Text("No Supervisors Online Currently.")]
+                              : _supervisors,
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Divider(
+              height: 1,
+              indent: 20,
+              endIndent: 20,
+              thickness: 1,
+            ),
+            Expanded(
+              flex: 0,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Name:",
+                      style: TextStyle(fontSize: 24.0),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                          controller: _nameController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5.0)),
+                                borderSide:
+                                    BorderSide(color: Colors.lightBlue)),
+                            labelText: 'Name',
+                            hintText: 'Enter Your Name',
+                          )),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        if (_nameController.text.isNotEmpty) {}
+                      },
+                      child: const Text('Enter Queue'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
