629438298701809ac4e690a514a78c0dfcfa6b00
JSYRD <1353097778@qq.com>
2023-09-08 11:22:40 +0200

    feat: new module

---
+++ lib/config.dart
@@ -0,0 +1,3 @@
+class Config {
+  static String url = "tcp://ds.iit.his.se:5555";
+}
---
+++ lib/listinfo.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ListInfo extends Padding {
+  ListInfo({Key? key, required String title, required String data})
+      : super(
+            key: key,
+            padding: const EdgeInsets.all(20.0),
+            child: OutlinedButton(
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 16),
+                          child: Text(
+                            title,
+                            style: const TextStyle(
+                                color: Colors.black, fontSize: 20),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 16),
+                          child: Text(
+                            data,
+                            style: const TextStyle(
+                                color: Colors.grey, fontSize: 15),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                )));
+}
