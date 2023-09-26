b32310ebe9915b262efaa73289fdcaa3e865fd39
JSYRD <1353097778@qq.com>
2023-09-27 01:11:46 +0200

    feat: format listenurl and replyurl with base

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,9 @@
 class Config {
-  static String listenUrl = "tcp://ds.iit.his.se:5555";
-  static String replyUrl = "tcp://ds.iit.his.se:5556";
+  static String baseUrl = "tcp://ds.iit.his.se";
+  static String listenUrl = "tcp://$baseUrl:5555";
+  static String replyUrl = "tcp://$baseUrl:5556";
+  // static String listenUrl = "tcp://10.5.16.205:5555";
+  // static String replyUrl = "tcp://10.5.16.205:5556";
+  // static String listenUrl = "tcp://localhost:5555";
+  // static String replyUrl = "tcp://localhost:5556";
 }
